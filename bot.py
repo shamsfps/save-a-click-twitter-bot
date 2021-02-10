71dd13ed78176b5793c942cc91001355c997b110
Adham Shams <adham20011@gmail.com>
2021-02-11 01:08:58 +0200

    v2.3

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 media_ids = []
 
