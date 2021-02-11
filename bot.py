93fbf692e07da0f96279d347164c782b81ebf034
Adham Shams <adham20011@gmail.com>
2021-02-11 13:19:30 +0200

    v3.0

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import tweepy
 import time
 import random
+import dropbox
 import os
 from os import environ
 from selenium import webdriver
@@ -15,6 +16,8 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
 
+dbx = dropbox.Dropbox(environ['TOKEN'])
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -25,36 +28,37 @@ driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), o
 
 media_ids = []
 
-FILE_NAME = 'last_seen.txt'
+file = 'last_seen.txt'
+file_location = f"/Last Seen ID/{file}"
 
-def read_last_seen(FILE_NAME):
-    file_read = open(FILE_NAME, 'r')
-    last_seen_id = int(file_read.read().strip())
-    file_read.close()
-    return last_seen_id
+def read_file(dbx, file):
+    _,f = dbx.files_download(file)
+    f = f.content
+    f = f.decode("utf-8")
+    return f
 
-def store_last_seen(FILE_NAME,last_seen_id):
-    file_write = open(FILE_NAME,'w')
-    file_write.write(str(last_seen_id))
-    file_write.close()
-    return
+def upload_file(dbx, file_location, file):
+    with open(file, "rb") as :
+        dbx.files_upload(f.read(),file_location,mode=dropbox.files.WriteMode.overwrite)
 
 def reply():
-    tweets = api.mentions_timeline(read_last_seen(FILE_NAME),tweet_mode='extended')
+    tweets = api.mentions_timeline(read_file(dbx, file_location),tweet_mode='extended')
     for tweet in reversed(tweets):
         url = get_url(tweet)
         if url != "":
-            store_last_seen(FILE_NAME,tweet.id)
             driver.get(url)
             S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
             driver.set_window_size(S('Width'),S('Height')-(S('Height')*0.4)) # May need manual adjustment
             driver.find_element_by_tag_name('body').screenshot('screenshot.png')
             driver.quit()
+
             media = api.media_upload('screenshot.png')
             media_ids.append(media.media_id)
             api.update_status('@'+tweet.user.screen_name, tweet.id, media_ids=media_ids)
             os.remove('screenshot.png')
             media_ids.clear()
+
+            upload_file(dbx, file_location, file)
 
 def get_url(tweet):
     if tweet.in_reply_to_status_id is None:
