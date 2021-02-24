660f23a63d0ec45e2572df3ee73d42a9c6ff53e7
Adham Shams <adham20011@gmail.com>
2021-02-24 21:31:36 +0200

    v5.0

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--start-maximized')
-chrome_options.add_experimental_option("prefs", {"profile.block_third_party_cookies": True})
 
 media_ids = []
 
